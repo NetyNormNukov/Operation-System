611c9de6e0c98477af2d4abb5150b6eaa79dde25
NetyNormNukov <oboraroman2018@gmail.com>
2020-12-06 20:44:01 +0200

    Done Lab8 Task2

---
+++ src_C/Lesson8/Task2/main.c
@@ -0,0 +1,135 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+typedef struct
+{
+	pthread_cond_t cond;
+	pthread_mutex_t mutex;
+	int data;
+}dataThread;
+
+void seed() {
+	struct timespec now;
+	if (clock_gettime(CLOCK_REALTIME, &now) < 0) {
+		fprintf(stderr, "Error with time function!\n");
+		exit(EXIT_FAILURE);
+	}
+	srand(now.tv_nsec);
+}
+
+void producerFunc(void* arg)
+{
+	dataThread* data = (dataThread*)arg;
+	// printf("int data: %d\n",data->data);
+	while(1)
+	{
+		int r = 1+rand()%100;
+		pthread_mutex_lock(&(data->mutex));
+		data->data = r;
+		pthread_cond_signal(&(data->cond));
+		pthread_mutex_unlock(&(data->mutex));
+		sleep(1);
+	}
+}
+
+void consumerFunc(void* arg)
+{
+	dataThread* data = (dataThread*)arg;
+	// printf("int data2: %d\n",data->data);
+	while(1)
+	{
+		int read;
+		pthread_mutex_lock(&(data->mutex));
+		pthread_cond_wait(&(data->cond),&(data->mutex));
+		read = data->data;
+		pthread_mutex_unlock(&(data->mutex));
+		printf("%d\n", read);
+		sleep(1);
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	setbuf(stdout,NULL);
+	int delay = 15;
+	if (argc == 2)
+	{
+		delay = atoi(argv[1]);
+		printf("Value of delay = %d\n", delay);
+		printf("\n");
+	}
+	else
+	{
+		fprintf(stderr, "Cant receive value of delay\n" );
+		printf("Value of delay = 15\n");
+	}
+	sleep(1);//for normal print string:Value of delay.
+	//init data, cond, mutex;
+	dataThread* data = (dataThread*)malloc(sizeof(dataThread));
+	if (pthread_cond_init(&(data->cond),NULL) != 0)
+	{
+		fprintf(stderr, "Error pthread_cond_init()\n");
+		return EXIT_FAILURE;
+	}
+	if(pthread_mutex_init(&(data->mutex),NULL) != 0)
+	{
+		fprintf(stderr, "Error pthread_mutex_init()\n" );
+		return EXIT_FAILURE;
+	}
+	data->data = 0;
+
+	pthread_t tidProd;
+	pthread_t tidCons;
+	seed();
+	//create threads;
+	if(pthread_create(&tidProd,NULL,&producerFunc,data) != 0)
+	{
+		fprintf(stderr, "Error pthread_create()\n");
+		return EXIT_FAILURE;
+	}
+	if(pthread_create(&tidCons,NULL,&consumerFunc,data) != 0)
+	{
+		fprintf(stderr, "Error pthread_create()\n");
+		return EXIT_FAILURE;
+	}
+	sleep(delay);
+	//cancel threads
+	if (pthread_cancel(tidProd) != 0)
+	{
+		fprintf(stderr, "Cant cancel thread\n" );
+		sleep(3);
+		pthread_cancel(tidProd);
+		return EXIT_FAILURE;
+	}
+	if (pthread_cancel(tidCons) != 0)
+	{
+		fprintf(stderr, "Cant cancel thread\n" );
+		sleep(3);
+		pthread_cancel(tidCons);
+		return EXIT_FAILURE;
+	}
+	
+	//wait threads
+	if(pthread_join(tidProd,NULL) != 0)
+	{
+		fprintf(stderr, "Join error\n");
+	}
+	if(pthread_join(tidCons,NULL) != 0)
+	{
+		fprintf(stderr, "Join error\n");
+	}
+	//free mem
+	if(pthread_cond_destroy(&(data->cond)) != 0)
+	{
+		fprintf(stderr, "Some thread still work with cond\n" );
+	}
+	if(pthread_mutex_destroy(&(data->mutex)) != 0)
+	{
+		fprintf(stderr, "Some thread still work with mutex\n" );
+	}
+	free(data);
+	printf("The end)\n");
+	return EXIT_SUCCESS;
+}
