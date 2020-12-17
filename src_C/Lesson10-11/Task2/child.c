06cd631cda696a8799d502be59c57036bba54d65
NetyNormNukov <oboraroman2018@gmail.com>
2020-12-17 10:58:29 +0200

    little change Lab10-11 Task2

--- src_C/Lesson10-11/Task2/child.c
+++ src_C/Lesson10-11/Task2/child.c
@@ -20,75 +20,54 @@ union semnum {
 
 int main(int argc, char** argv) {
     key_t token = ftok(SHM_KEY, 1);
-    int shm_handler = shmget(token, 128, IPC_CREAT | IPC_EXCL | 0660);
-    int sem_handler = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | 0660);
+    // printf("%d\n", atoi(argv[2]));
+    // printf("%s\n", );
+
+    int shm_handler = shmget(token, 128, IPC_CREAT | 0660);
+    int sem_handler = semget(SEM_KEY, 1, IPC_CREAT | 0660);
     unsigned short sem_vals[1];
     struct semid_ds ds;
     struct sembuf sb[1];
     if (shm_handler == -1) {
-        printf("Parent: Can't create shared memory!\n");
+        printf("Child: Can't create shared memory!\n");
         // return 0;
     }
     if (sem_handler == -1) {
-        printf("Parent: can't create semaphore!\n");
+        printf("Child: can't create semaphore!\n");
         // return 0;
     }
     int* buffer = shmat(shm_handler, NULL, 0);
     if (buffer == (int*) - 1) {
-        printf("Parent: Can't get memory address!\n");
+        printf("Child: Can't get memory address!\n");
     }
 
-    sem_vals[0] = 1;
-    sem_arg.array = sem_vals;
-    if (semctl(sem_handler, 0, SETALL, sem_arg) == -1) {
-        printf("Parent: semctl() error!\n");
-        return 0;
-    }
-
-    //close semaphore
-    sb[0].sem_flg = 0;
-    sb[0].sem_num = 0;
-    sb[0].sem_op = -1;
-    semop(sem_handler, sb, 1);
-
-    pid_t pid  = fork();
-    if (pid == 0) {
-        char* args[] = {"./child", "./child", NULL};
-        execv("./child", args);
-        printf("Error: can't create process!\n");
-    } else {
-        while (1) {
-            int n;
-            printf("Enter n\n");
-            scanf("%d", &n);
-            buffer[0] = n;
-            if (buffer[0] == -1)
+    int sum = 0;
+    while (1) {
+        sum = 0;
+        //close semaphore
+        sb[0].sem_flg = 0;
+        sb[0].sem_num = 0;
+        sb[0].sem_op = -1;
+        semop(sem_handler, sb, 1);
+        int n = buffer[0];
+        if (n == -1)
+        {
+            if (semctl(sem_handler, 0, IPC_RMID, sem_arg) == -1)
             {
-                if (semctl(sem_handler, 0, IPC_RMID, sem_arg) == -1)
-                {
-                    printf("Parent: semctl() error!\n");
-                    return 0;
-                }
-                shmctl(shm_handler, IPC_STAT, &ds);
-
+                printf("Parent: semctl() error!\n");
+                return 0;
             }
-            printf("Enter n numbers!\n");
-            for (int i = 1; i <= n; i++) {
-                scanf("%d", &buffer[i]);
-            }
-            sb[0].sem_flg = 0;
-            sb[0].sem_num = 0;
-            sb[0].sem_op = 1;
-            semop(sem_handler, sb, 1);
-            usleep(100);
-            sb[0].sem_flg = 0;
-            sb[0].sem_num = 0;
-            sb[0].sem_op = -1;
-            semop(sem_handler, sb, 1);
-            printf("Answer is %d\n", buffer[0]);
+            shmctl(shm_handler, IPC_STAT, &ds);
         }
-
+        for (int i = 1; i < n + 1; i++)
+            sum += buffer[i];
+        buffer[0] = sum;
+        sb[0].sem_flg = 0;
+        sb[0].sem_num = 0;
+        sb[0].sem_op = 1;
+        semop(sem_handler, sb, 1);
     }
+
 
     return 0;
 }
